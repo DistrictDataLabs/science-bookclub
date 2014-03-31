e39e6d8af97f93b0c43e4efb1c78f54cfabc6a15
Benjamin Bengfort <benjamin@bengfort.com>
2014-03-31 12:13:36 -0400

    API for accessing Goodreads

---
+++ octavo/ingest/__init__.py
@@ -0,0 +1,18 @@
+# octavo.ingest
+# Handles ingesting data from the Goodreads API
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Mon Mar 31 09:37:55 2014 -0400
+#
+# Copyright (C) 2014 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Handles ingesting data from the Goodreads API
+"""
+
+##########################################################################
+## Imports
+##########################################################################
