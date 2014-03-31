bd327ab405ebd0b747fc32308fc1c337e24f9805
Benjamin Bengfort <benjamin@bengfort.com>
2014-03-30 20:35:40 -0400

    one more comment

---
+++ octavo/__init__.py
@@ -0,0 +1,18 @@
+# octavo
+# A recommender library for Book discovery
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Sun Mar 30 20:35:15 2014 -0400
+#
+# Copyright (C) 2014 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+A recommender library for Book discovery
+"""
+
+##########################################################################
+## Imports
+##########################################################################
