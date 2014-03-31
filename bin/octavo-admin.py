c53c76581f7e1cb602a9d5ba22a54f4c9d33bad2
Benjamin Bengfort <benjamin@bengfort.com>
2014-03-31 13:03:31 -0400

    ingest command

--- bin/octavo-admin.py
+++ bin/octavo-admin.py
@@ -23,12 +23,34 @@ import os
 import sys
 import argparse
 
+from octavo.ingest.goodreads import Goodreads
+
 ##########################################################################
 ## Command Line Variables
 ##########################################################################
 
 DESCRIPTION = "An administrative utility for the Science bookclub"
 EPILOG      = "If there are any bugs or concerns, please comment on Github"
+
+##########################################################################
+## Administrative Commands
+##########################################################################
+
+def ingest(args):
+    print "Fetching data from Goodreads"
+    api = Goodreads(apikey=args.apikey, htdocs=args.htdocs)
+    count = 0
+    for userid in args.userid:
+        fname = "%s.xml" % str(userid)
+        paths = api.reviews(userid, fname, batch=args.batch)
+        fmtst = "  fetched %s"
+
+        if isinstance(paths, basestring): paths = [paths]
+        for path in paths:
+            count += 1
+            print fmtst % path
+
+    return "%i reviews downloaded to %s\n" % (count, api.htdocs)
 
 ##########################################################################
 ## Main Method
@@ -38,12 +60,23 @@ def main(*argv):
 
     # Construct the argument parser
     parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
+    subparsers = parser.add_subparsers(title='commands', description='Administrative commands for Octavo')
 
-    # Add command line arguments
-    # TODO
+    # Ingest Reviews Command
+    ingest_parser = subparsers.add_parser('ingest', help='Collect the reviews for a Goodreads userid')
+    ingest_parser.add_argument('--batch', action='store_true', default=False, help='force paginated download')
+    ingest_parser.add_argument('--htdocs', type=str, default=None, help='set the download directory')
+    ingest_parser.add_argument('--apikey', type=str, default=None, help='goodreads API key')
+    ingest_parser.add_argument('userid', type=int, nargs='+', help='userid to collect reviews for')
+    ingest_parser.set_defaults(func=ingest)
 
-    # Parse the arguments from the commandline
-    options = parser.parse_args()
+    # Handle input from the command line
+    args = parser.parse_args()            # Parse the arguments
+    try:
+        msg = args.func(args)             # Call the default function
+        parser.exit(0, msg)               # Exit clearnly with message
+    except Exception as e:
+        parser.error(str(e))              # Exit with error
 
 if __name__ == '__main__':
     main(*sys.argv)
