61cd7cfaa8b25e9e77a6dcb6ae0489b65b20f903
Benjamin Bengfort <benjamin@bengfort.com>
2014-03-31 13:39:38 -0400

    prepared to xml wrangle

---
+++ octavo/wrangle/__init__.py
@@ -0,0 +1,18 @@
+# octavo.wrangle
+# Data wrangling for Octavo App
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Mon Mar 31 13:26:02 2014 -0400
+#
+# Copyright (C) 2014 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Data wrangling for Octavo App
+"""
+
+##########################################################################
+## Imports
+##########################################################################
