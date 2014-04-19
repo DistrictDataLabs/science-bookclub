8077334bd0f67a851c4092c0bfbd8c91cafbceee
Benjamin Bengfort <benjamin@bengfort.com>
2014-04-19 10:18:32 -0400

    reporting tools

---
+++ octavo/report/__init__.py
@@ -0,0 +1,20 @@
+# octavo.report
+# Reporting the results of our recommender
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Fri Apr 11 17:11:22 2014 -0400
+#
+# Copyright (C) 2014 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Reporting the results of our recommender
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+from base import *
